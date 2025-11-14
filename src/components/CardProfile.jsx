4cd30a6a38318ef3d191c3c0138780e0c23c455a
FrostLeaf15 <85655364+FrostLeaf15@users.noreply.github.com>
2025-11-14 10:04:30 +0700

    Update

--- src/components/CardProfile.jsx
+++ src/components/CardProfile.jsx
@@ -3,13 +3,15 @@ import LinkSocial from './LinkSocial'
 function CardProfile({ nama, address, skill }) {
     return (
         <>
-            <section className="bg-[#1A3D64] rounded-2xl p-8 text-[#f4f4f4] flex flex-col justify-center items-center">
-                <img className='rounded-full' src="https://dummyimage.com/250x250/000/fff" alt="Pic Profile" />
+            <section className="bg-[#1f1f1f] rounded-2xl p-8 text-[#f4f4f4] flex flex-col justify-center items-center">
+                <img className='rounded-full' src="/src/img/avatar-jessica.jpeg" alt="Pic Profile" />
                 <h1 className="font-bold text-3xl mt-2">{nama}</h1>
-                <h2 className="font-semibold text-2xl text-yellow-300">{address}</h2>
+                <h2 className="font-semibold text-2xl text-[#79941e]">{address}</h2>
                 <p className="mt-6">{skill}</p>
                 <LinkSocial link="Github" />
-                <LinkSocial link="LinkedIn" />
+                <LinkSocial link="Frontend Mentor" />
+                <LinkSocial link="LinkedId" />
+                <LinkSocial link="Twitter" />
                 <LinkSocial link="Instagram" />
             </section>
         </>
--- src/components/LinkSocial.jsx
+++ src/components/LinkSocial.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function LinkSocial({ link }) {
     return (
-        <section className='bg-[#1D546C] font-semibold w-full mt-4 py-2 rounded-2xl cursor-pointer hover:bg-sky-500 hover:text-black transition duration-300'>
+        <section className='bg-[#333333] font-semibold w-full mt-4 py-2 rounded-2xl cursor-pointer hover:bg-[#555555] hover:text-black transition duration-300'>
             <div>{link}</div>
         </section>
     )
