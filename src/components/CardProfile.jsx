5d68810c4bdcac4f0d857e1409b9195bd8cbcaed
FrostLeaf15 <85655364+FrostLeaf15@users.noreply.github.com>
2025-11-17 12:26:01 +0700

    Update

--- src/components/CardProfile.jsx
+++ src/components/CardProfile.jsx
@@ -4,7 +4,7 @@ function CardProfile({ nama, address, skill }) {
     return (
         <>
             <section className="bg-[#1f1f1f] rounded-2xl p-8 text-[#f4f4f4] flex flex-col justify-center items-center">
-                <img className='rounded-full' src="/src/img/avatar-jessica.jpeg" alt="Pic Profile" />
+                <img className='rounded-full' src="/img/avatar-jessica.jpeg" alt="Pic Profile" />
                 <h1 className="font-bold text-3xl mt-2">{nama}</h1>
                 <h2 className="font-semibold text-2xl text-[#79941e]">{address}</h2>
                 <p className="mt-6">{skill}</p>
